8bbb419e014dc7e52cfb79b9ce38b7f814f9c10a
설설이 <leesugar90@gmail.com>
2020-05-26 22:16:52 +0900

    no message

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var app = require('./config/express')();
-var passport = require('./config/passport')(app);
-var auth = require('./routes/auth')(passport);
-app.use('/auth/', auth);
+var app = require("./config/express")();
+var passport = require("./config/passport")(app);
+var auth = require("./routes/auth")(passport);
+app.use("/auth/", auth);
 
-var topic = require('./routes/topic')();
-app.use('/topic', topic);
+var topic = require("./routes/topic")();
+app.use("/topic", topic);
 
-app.listen(3000, function(){
-    console.log('Connected 3000 port!');
+app.listen(3000, function() {
+  console.log("Connected 3000 port!");
 });
